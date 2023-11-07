5e5598d77dce1f76502fd78e787b6ba4da06b6fe
[enzo] <[enzolocquet@gmail.com]>
2023-11-07 10:54:33 +0100

    ds

--- src/Controller/DistrictController.php
+++ src/Controller/DistrictController.php
@@ -6,7 +6,12 @@ use App\Repository\CategorieRepository;
 use App\Repository\PlatsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Utilisateur;
+use Symfony\Component\Security\Core\Security;
 
 class DistrictController extends AbstractController
 {
@@ -72,5 +77,56 @@ class DistrictController extends AbstractController
             'plats' => $plats,
         ]);
     }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+        
+            // Créez un nouvel objet Utilisateur et enregistrez les données du formulaire
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNom($formData['nom']);
+            $utilisateur->setPrenom($formData['prenom']);
+            $utilisateur->setTelephone($formData['telephone']);
+            $utilisateur->setEmail($formData['email']);
+            $utilisateur->setPassword($formData['password']);
+            $utilisateur->setVille($formData['ville']);
+            $utilisateur->setAdresse($formData['adresse']);
+            $utilisateur->setCp($formData['code_postal']);
+            
+            // Utilisez le rôle de l'utilisateur connecté
+            $connectedUser = $security->getUser();
+            $roles = $connectedUser->getRoles();
+            $rolesAsString = implode(',', $roles);
+            $utilisateur->setRoles($rolesAsString);
+            
+            // Utilisez l'EntityManager pour persister et flush l'objet Utilisateur
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+        
+            // Redirigez l'utilisateur vers la page de succès après la soumission réussie.
+            return $this->redirectToRoute('app_success');
+        }
+        
+        return $this->render('district/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    
+    #[Route('/contact/success', name: 'app_success')]
+    public function success(): Response
+    {
+        // Cette action affiche une page de succès après une soumission réussie.
+        return $this->render('district/success.html.twig');
+    }
+    
+
+
 
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('telephone', TelType::class, [ 
+                'label' => 'Téléphone',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                ],
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville',
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse',
+            ])
+            ->add('code_postal', TextType::class, [
+                'label' => 'Code postal',
+            ]);
+            
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
