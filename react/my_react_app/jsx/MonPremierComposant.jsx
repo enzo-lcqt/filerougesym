609999bed5b428f74ddac659be44f931c983b255
[enzo] <[enzolocquet@gmail.com]>
2023-11-28 14:08:33 +0100

    fds

--- react/my_react_app/jsx/MonPremierComposant.jsx
+++ react/my_react_app/jsx/MonPremierComposant.jsx
@@ -1,60 +1,60 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Card, CardImg, Spinner } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 
 const MonPremierComposant = (props) => {
-    //etat de l'animation du chargement des donnees
-    const [loading,setLoading] = useState(true);
-    const [users, setUsers] = useState([]);
+    // État de l'animation du chargement des données
+    const [loading, setLoading] = useState(true);
+    const [categories, setCategories] = useState([]);
 
-    //on va appeler cette api pour récupérer un tableau d'utilisateurs 
-    useEffect( () => {
-        axios.get("https://reqres.in/api/users?per_page=20").then((response)=>{
-            setUsers(response.data.data);
-            // console.log(response.data.data);
+    // Fonction pour récupérer les catégories de votre base de données
+    const fetchCategories = async () => {
+        try {
+            // Remplacez l'URL par celle de votre API pour récupérer les catégories
+            const response = await fetch("https://127.0.0.1:8000/api/categories?page=1");
+            const data = await response.json();
+            setCategories(data['hydra:member']);
             setLoading(false);
-        }).catch(thrown => {});
+        } catch (error) {
+            console.error("Erreur lors de la récupération des catégories", error);
+        }
+    };
 
+    // Appeler la fonction fetchCategories lors du montage du composant
+    useEffect(() => {
+        fetchCategories();
     }, []);
 
     return (
         <div className={"container"}>
-            {
-                users ? (
-                    <>
-                        <div className="row">
-                            <div className={'col-12 mt-4'}>
-                                <h1 className={'p-3 text-center'} style={{color: "#4169E1", fontSize: "2.8em"}}>Notre équipe</h1>
-                                <hr />
+            {loading ? (
+                <Spinner animation="grow" variant="info" />
+            ) : (
+                <>
+                    <div className="row">
+                        <div className={'col-12 mt-4'}>
+                            <h1 className={'p-3 text-center'} style={{ color: "#4169E1", fontSize: "2.8em" }}>Catégories</h1>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        {categories.map((category, index) => (
+                            <div className="col-md-4 p-2" key={index}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={category.image} />
+                                    <Card.Body>
+                                        <Card.Title>{category.libelle}</Card.Title>
+                                        <Card.Text>
+                                            {category.description}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
                             </div>
-                        </div>
-                        <div className="row">
-
-                            {
-                                users && users.map((item, index)=> (
-                                    <div className="col-md-4 p-2" key={index}>
-                                        <Card style={{ width: '18rem' }}>
-                                            <Card.Img variant="top" src={item.avatar} />
-                                            <Card.Body>
-                                                <Card.Title>{item.first_name} {item.last_name}</Card.Title>
-                                                <Card.Text>
-                                                    {item.email}
-                                                </Card.Text>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>)
-                                )}
-
-                        </div>
-                    </>
-                ) : (
-                    <Spinner animation="grow" variant="info" />
-                )
-            }
-
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
+};
 
-}
-
-export default MonPremierComposant
+export default MonPremierComposant;
